03037f7fa8648a7e7eabec46c6d9b8dcdd8e5ec3
satoumasato <masakun1993@gmail.com>
2021-03-11 18:19:57 +0900

    編集機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
 
-  @posts = Post.all
+  @posts = Post.order(id: :asc)
   end
 
   def show
@@ -20,12 +20,21 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+
   end
 
   def update
+    post = Post.find(params[:id])
+    post.update!(post_params)
+    redirect_to post
+
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy!
+    redirect_to root_path
   end
 
   private
